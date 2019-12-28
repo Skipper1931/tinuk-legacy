44c1aecf24762ca11ad10fe826455d4bef13a34c
Skipper1931 <Skipper1931@users.noreply.github.com>
2019-12-28 16:00:25 -0500

    Updated qemu.sh to use Curses UI if there's no GUI (such as in an SSH terminal)

--- qemu.sh
+++ qemu.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom tinuk.iso -d guest_errors
+if [ -z $DISPLAY ]; then # if we're not in a GUI go to Curses UI for QEMU
+	export QEMUCurses="--curses"
+fi
+
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom tinuk.iso -d guest_errors $QEMUCurses
 
 ./clean.sh
