f96cb2b4752a20aa3424a32dc865239cf4ebb0a6
Skipper1931 <Skipper1931@users.noreply.github.com>
2019-12-30 10:38:59 -0500

    Added multiboot header and magic val as kmain() params

--- kernel/boot/kmain.cpp
+++ kernel/boot/kmain.cpp
@@ -3,12 +3,14 @@
 
 #include <stdio.h>
 #include <kernel/include/tty.h>
-#include <stack.h>
+#include <kernel/include/multiboot.h>
 
 extern "C"
 {
-	void kmain(void)
+	void kmain(multiboot_info_t* mb_info, unsigned int magic)
 	{
+		if (magic != 732803074) return; // magic value put by GRUB isn't valid, thus something went wrong
+		
 		tty_init();
 		printf("\n");
 
