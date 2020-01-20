3009944fb59dc5b2b0bf855408a0a868815db410
Skipper1931 <lordskipper1931@gmail.com>
2020-01-20 10:12:23 -0500

    Made GRUB mmap dump more readable

--- kernel/boot/kmain.cpp
+++ kernel/boot/kmain.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <kernel/include/tty.h>
 #include <kernel/include/multiboot.h>
+#include <string.h>
 
 bool get_mbi_flag(multiboot_info_t* mbi, uint8_t bit) {
 	return (mbi->flags >> (12-bit)) & 1;
@@ -27,10 +28,33 @@ extern "C"
 
 		multiboot_memory_map_t* mmap = (multiboot_memory_map_t*)mb_info->mmap_addr;
 		while ((unsigned int)mmap < mb_info->mmap_addr + mb_info->mmap_length) {
-			printf("Addr: 0x%llx   ", mmap->addr);
+			printf("Addr: 0x%llx  ", mmap->addr);
 			printf("Length: 0x%llx  ", mmap->len);
-			printf("Type: 0x%x", mmap->type);
-			printf("\n");
+
+			char type_msg[6];
+
+			switch (mmap->type)
+			{
+			case 1:
+				memcpy(type_msg, "AVAIL", 6);
+				break;
+			case 3:
+				memcpy(type_msg, "ACPIR", 6);
+				break;
+			case 4:
+				memcpy(type_msg, "HIBER", 6);
+				break;
+			case 5:
+				memcpy(type_msg, "BADRM", 6);
+				break;
+			
+			default:
+				memcpy(type_msg, "RESRV", 6);
+				break;
+			}
+
+			printf("Type: %s", type_msg);
+			printf(" (0x%x)\n", mmap->type);
 
 			mmap = (multiboot_memory_map_t*) ((unsigned int)mmap + (mmap->size + 4)); // mmap->size doesn't take itself into account so it (32-bit so 4 bytes) needs to be added
 		}
