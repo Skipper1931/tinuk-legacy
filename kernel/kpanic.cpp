d95fcc8a14139a3cdeccd2288fb244d24475ff37
skipper1931 <lordskipper1931@gmail.com>
2020-02-01 12:12:31 -0500

    Added rudimentary kpanic()
    Just prints whatever you feed it and exits.

---
+++ kernel/kpanic.cpp
@@ -0,0 +1,9 @@
+#include <kernel/include/panic.h>
+#include <stdio.h>
+
+extern "C" void asm_kpanic(); // links the asm_kpanic func in arch/i386/boot.S to this
+
+void kpanic(const char* msg) {
+    printf("KERNEL PANIC: %s\n", msg);
+    asm_kpanic();
+}
