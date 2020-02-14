bca3f42100359f005514af23de3443977f6bbfff
skipper1931 <lordskipper1931@gmail.com>
2020-02-13 20:59:06 -0500

    Put sysroot on the ISO instead of isodir
    Also copied the source code onto the iso so if there's ever becomes an actual OS the source code of it will already be on the system so the GNU GPL doesn't come after me. Idk man i felt like it okay dont hate on me

--- iso.sh
+++ iso.sh
@@ -2,15 +2,19 @@
 set -e
 . ./build.sh
  
-mkdir -p isodir
-mkdir -p isodir/boot
-mkdir -p isodir/boot/grub
- 
-cp sysroot/boot/karyon.kernel isodir/boot/karyon.kernel
-cat > isodir/boot/grub/grub.cfg << EOF
+mkdir -p sysroot/boot/grub
+mkdir -p sysroot/sys/src
+
+cp -r kernel sysroot/sys/src
+cp -r libc sysroot/sys/src
+
+find sysroot/sys/src -type f -name "*.d" -delete
+find sysroot/sys/src -type f -name "*.o" -delete
+
+cat > sysroot/boot/grub/grub.cfg << EOF
 menuentry "TinukOS" {
 	insmod /boot/karyon.kernel
 	multiboot /boot/loader32.elf
 }
 EOF
-grub-mkrescue -o tinuk.iso isodir
+grub-mkrescue -o tinuk.iso sysroot
