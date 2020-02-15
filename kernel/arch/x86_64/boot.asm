ce8a2f24d5fe20af272e7390f95cb4d965c606cf
Skipper1931 <lordskipper1931@gmail.com>
2020-02-14 19:42:22 -0500

    Modified sections so _start64 is at the beginning of the kernel binary
    So i don't need to find it myself.
    
    I mean i'll probably parse the ELF stuff to find it anyway but this'll probably help

--- kernel/arch/x86_64/boot.asm
+++ kernel/arch/x86_64/boot.asm
@@ -1,9 +1,8 @@
 BITS 64
 
 ; The kernel entry point.
-SECTION .text
+SECTION .entry
 GLOBAL _start64
-GLOBAL asm_kpanic
 EXTERN _init
 EXTERN kmain
 
@@ -21,6 +20,10 @@ _start64:
 	; Transfer control to the main kernel.
 	lea rax, [rel kmain]
 	call rax
+
+
+SECTION .text
+GLOBAL asm_kpanic
 
 asm_kpanic:
 	; Hang if kmain unexpectedly returns OR if we call kpanic.
